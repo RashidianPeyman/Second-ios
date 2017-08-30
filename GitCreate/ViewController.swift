bf1180a11e8ef5cf409c3f74d92af6f48989bdbb
peyman <peyman7627@peymans-Mac.local>
2017-08-29 23:25:06 -0700

    Initial Commit

---
+++ GitCreate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitCreate
+//
+//  Created by peyman on 8/29/17.
+//  Copyright © 2017 peyman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
